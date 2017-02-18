861d0249612c1d08f6638c246986ddc7dc8059d1
Matthew Preucil <mattpsbs@gmail.com>
2017-02-18 14:06:20 -0800

    Add a CLI build scripts.

---
+++ scripts/build.sh
@@ -0,0 +1,30 @@
+#! /bin/bash
+
+BASEDIR="$(dirname "$0")"
+
+pushd $BASEDIR/../servers/content >> /dev/null
+echo "Building content-server from $(pwd)..."
+swift build
+popd >> /dev/null
+
+pushd $BASEDIR/../servers/meta >> /dev/null
+echo "Building meta-server from $(pwd)..."
+swift build
+popd >> /dev/null
+
+pushd $BASEDIR/../cli >> /dev/null
+echo "Building CLI from $(pwd)..."
+swift build
+popd >> /dev/null
+
+pushd $BASEDIR >> /dev/null
+echo "Installing..."
+rm -rf bin
+mkdir bin
+cd bin
+ln -s ../cli/.build/debug/amethyst amethyst
+ln -s ../servers/content/.build/debug/content-server content-server
+ln -s ../servers/meta/.build/debug/meta-server meta-server
+popd >> /dev/null
+
+echo "Success!"
