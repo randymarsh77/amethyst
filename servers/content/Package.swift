23411c6e36e982ae058fb4cda517873a34e83da5
Matthew Preucil <mattpsbs@gmail.com>
2017-02-18 09:40:38 -0800

    Make servers swift packages.
    swift build works for building executables.

---
+++ servers/content/Package.swift
@@ -0,0 +1,10 @@
+import PackageDescription
+
+let package = Package(
+    name: "content-server",
+    dependencies: [
+		.Package(url: "https://github.com/randymarsh77/bonjour", majorVersion: 1),
+		.Package(url: "https://github.com/randymarsh77/crystal", majorVersion: 0),
+		.Package(url: "https://github.com/randymarsh77/using", majorVersion: 1),
+	]
+)
---
+++ servers/meta/Package.swift
@@ -0,0 +1,13 @@
+import PackageDescription
+
+let package = Package(
+    name: "meta-server",
+    dependencies: [
+		.Package(url: "https://github.com/randymarsh77/bonjour", majorVersion: 1),
+		.Package(url: "https://github.com/randymarsh77/itunes", majorVersion: 0),
+		.Package(url: "https://github.com/randymarsh77/inmemorycache", majorVersion: 0),
+		.Package(url: "https://github.com/randymarsh77/using", majorVersion: 1),
+		.Package(url: "https://github.com/vapor/redbird", majorVersion: 0),
+		.Package(url: "https://github.com/vapor/vapor", majorVersion: 1, minor: 5),
+	]
+)
